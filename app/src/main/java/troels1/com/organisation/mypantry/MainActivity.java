7f3e75cac4335de6b439358b8c30727495e46626
TroelsVIA <VGEe3245h>
2022-04-09 10:11:21 +0200

    First commit

---
+++ app/src/main/java/troels1/com/organisation/mypantry/MainActivity.java
@@ -0,0 +1,14 @@
+package troels1.com.organisation.mypantry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
