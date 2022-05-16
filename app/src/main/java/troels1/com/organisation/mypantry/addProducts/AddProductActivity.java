6fac5f4820443c99cfd626396e7beda2dd06b84a
1Miju <miju@cphmed.com>
2022-05-16 10:42:22 +0200

    Implementering af lists4

--- app/src/main/java/troels1/com/organisation/mypantry/addProducts/AddProductActivity.java
+++ app/src/main/java/troels1/com/organisation/mypantry/addProducts/AddProductActivity.java
@@ -4,14 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import troels1.com.organisation.mypantry.R;
 import troels1.com.organisation.mypantry.mainListVIew.MenuActivity;
 import troels1.com.organisation.mypantry.myShoppingLists.MyShoppingListActivity;
+import troels1.com.organisation.mypantry.myShoppingLists.MyShoppingListActivityViewModel;
 import troels1.com.organisation.mypantry.pantry.PantryActivity;
 
 public class AddProductActivity extends AppCompatActivity {
+
+    private Button tilføj;
+    private EditText productNavn;
+    private EditText productAntal;
+    private AddProductActivityViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,5 +46,21 @@ public class AddProductActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MenuActivity.class);
         startActivity(intent);
     });
+
+            tilføj = (Button)findViewById(R.id.tilføj);
+            productNavn = (EditText)findViewById(R.id.produktNavn);
+        productAntal = (EditText)findViewById(R.id.produktAntal);
+
+            tilføj.setOnClickListener(
+                    new View.OnClickListener()
+                    {
+                        public void onClick(View view)
+                        {
+                            Toast.makeText(this, "tryk på Virker", Toast.LENGTH_LONG).show();
+                        }
+
+
+                    });
+        }
 }
-}
+
---
+++ app/src/main/java/troels1/com/organisation/mypantry/addProducts/AddProductActivityViewModel.java
@@ -0,0 +1,19 @@
+package troels1.com.organisation.mypantry.addProducts;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class AddProductActivityViewModel extends AndroidViewModel {
+    
+    public AddProductActivityViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    public void insert() {
+    }
+
+    public void SendUserQuery() {
+    }
+}
